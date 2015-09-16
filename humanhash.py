3911268608e1adff253ce5f29171a9ebfa789081
Drew Hubl <drew.hubl+github@gmail.com>
2015-09-16 05:04:41 -0600

    Convert wordlist elements to normal, boring Python strings

--- humanhash.py
+++ humanhash.py
@@ -12,6 +12,7 @@ import sys
 if sys.version_info.major == 3:
     #Map returns an iterator in PY3K
     py3_map = map
+
     def map(*args, **kwargs):
         return [i for i in py3_map(*args, **kwargs)]
 
@@ -101,7 +102,7 @@ class HumanHasher(object):
         # Compress an arbitrary number of bytes to `words`.
         compressed = self.compress(bytes, words)
         # Map the compressed byte values through the word list.
-        return separator.join(self.wordlist[byte] for byte in compressed)
+        return separator.join(str(self.wordlist[byte]) for byte in compressed)
 
     @staticmethod
     def compress(bytes, target):
